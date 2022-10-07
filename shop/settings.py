db52ab8f8b0bcfaf4045e1dcb580b00f37509de2
JUMANAHASIN123 <ju123hasin@gmail.com>
2022-10-07 11:50:35 +0530

    2nd commit

--- shop/settings.py
+++ shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jtdilbep#t2ygn=v+v9iv1g#@7nv_2)e_ssj^@2gpz&s7o)2lx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
